9b83ccb6286c08c5617defbd6a978609c0f6389f
Grace Choi <oiograceoio@gmail.com>
2018-10-09 05:37:08 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -84,12 +84,11 @@ def adding_matz
         }
      }
 		 programmer_hash[:yukihiro_matsumoto] = [:known_for]
-		 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+		 programmer_hash[:yukihiro_matsumoto][:known_for] << "Ruby"
 		 programmer_hash[:yukihiro_matsumoto] = [:languages]
 		 programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
-		 puts programmer_hash
 end
-adding_matz
+
 def changing_alan
 	programmer_hash =
  		{
